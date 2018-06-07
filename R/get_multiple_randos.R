06ff148e306ae59d4f45efca672b9538b9c5f716
peppato <cfcardillo23@gmail.com>
2018-06-07 00:42:39 -0400

    added multiple random jokes function

---
+++ R/get_multiple_randos.R
@@ -0,0 +1,35 @@
+#' Get a bunch of random Chuck Norris jokes.
+#'
+#' @description Get a bunch of random Chuck Norris jokes.
+#' @param jokes The number of jokes to fetch.
+#' @return A tibble of jokes
+#' @examples get_multiple_randos(10)
+#' @export
+
+get_multiple_randos <- function(jokes = 5){
+
+  if(class(jokes) != "numeric"){
+
+    message("Please use an integer to specify joke count.")
+
+  } else{
+
+    jokes_round <- round(jokes)
+
+    random_joke_response <- httr::GET(stringr::str_c("http://api.icndb.com/jokes/random/", jokes_round, "?escape=javascript"))
+
+    if(random_joke_response$status_code == 200){
+
+      random_joke <- random_joke_response %>%
+        httr::content(as = "text", encoding = "UTF-8") %>%
+        fromJSON()
+
+      return(random_joke$value %>% dplyr::select(-categories))
+
+    } else {
+
+      message("Multiple randos unavailable..")
+
+    }
+  }
+}
