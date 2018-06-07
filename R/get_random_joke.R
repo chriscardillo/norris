0f35e90f722c14ac21c3718f89bb86a38d41307e
peppato <cfcardillo23@gmail.com>
2018-06-07 00:29:25 -0400

    added function for get_random_joke()

---
+++ R/get_random_joke.R
@@ -0,0 +1,26 @@
+#' Get a random Chuck Norris joke
+#'
+#' @description Get a random Chuck Norris joke
+#' @return A single joke
+#' @examples get_random_joke()
+#' @export
+
+get_random_joke <- function(){
+
+  random_joke_response <- httr::GET(stringr::str_c("http://api.icndb.com/jokes/random", "?escape=javascript"))
+
+  if(random_joke_response$status_code == 200){
+
+  random_joke <- random_joke_response %>%
+    httr::content(as = "text", encoding = "UTF-8") %>%
+    fromJSON()
+
+  return(random_joke$value$joke)
+
+  } else {
+
+    message("Random joke unavailable..")
+
+  }
+
+}
