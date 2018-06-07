565a3c467a6a7170d050c2a0c1318d2a3d280036
peppato <cfcardillo23@gmail.com>
2018-06-06 23:37:35 -0400

    first function -- get_joke_categories()

---
+++ R/get_joke_categories.R
@@ -0,0 +1,24 @@
+#' Get all available joke categories from ICNDB
+#'
+#' @return A vector of available joke categories
+#' @examples get_joke_categories()
+#' @export
+
+get_joke_categories <- function(){
+
+  categories_reponse <- httr::GET("http://api.icndb.com/categories")
+
+  if(categories_reponse$status_code == 200){
+
+  categories_unpacked <- categories_reponse %>%
+    httr::content(as = "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON()
+
+  return(categories_unpacked$value)
+
+  } else{
+
+    message("ICNDB joke categories unavailable..")
+
+  }
+}
